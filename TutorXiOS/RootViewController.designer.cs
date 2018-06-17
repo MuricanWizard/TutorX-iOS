b6a8412a97cf655ff2367c0c4e64bcd1b6c58e12
MuricanWizard <36910921+MuricanWizard@users.noreply.github.com>
2018-06-17 18:47:57 -0500

    Add project files.

---
+++ TutorXiOS/RootViewController.designer.cs
@@ -0,0 +1,14 @@
+﻿using Foundation;
+using System.CodeDom.Compiler;
+
+namespace TutorXiOS
+{
+	[Register ("RootViewController")]
+	partial class RootViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
+
